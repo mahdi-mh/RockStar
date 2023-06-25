3a6fec08821a2e8a9bc9050070a37c4de3835415
Mahdi Mohammadi <lord2350.mm@gmail.com>
2023-06-25 17:44:58 +0330

    Develop auth controller with register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Auth;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Validator;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
         $credentials = $request->only('email', 'password');
 
@@ -23,5 +26,30 @@ class AuthController extends Controller
         return response()->json([
             'token' => $token?->plainTextToken,
         ]);
+    }
+
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $data = $validator->getData();
+        $data['password'] = bcrypt($request->get('password'));
+
+        $user = User::create($data);
+
+        $token = $user?->createToken('Personal Access Token');
+
+        return response()->json([
+            'user' => $user->toArray(),
+            'token' => $token,
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
