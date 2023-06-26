33211b3136f39a7acf5eeee8e2e6310e633a98ca
Mahdi Mohammadi <lord2350.mm@gmail.com>
2023-06-26 23:56:24 +0330

    Write docs for product module

--- Modules/Product/Http/Controllers/ProductController.php
+++ Modules/Product/Http/Controllers/ProductController.php
@@ -2,16 +2,33 @@
 
 namespace Modules\Product\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Product\Models\Product;
+use Validator;
 
 class ProductController extends Controller
 {
     /**
+     * List
+     *
+     * List all products as paginated
+     *
      * @return mixed
+     * @group Product
+     *
+     * @urlParam per_page int The number of products per page.
+     * @urlParam page int The page number.
+     * @responseFile status=200 scenario="Success" Modules/Product/Storage/example-response/list-200.json
+     * @responseFile status=401 scenario="Unauthenticated" Modules/Product/Storage/example-response/list-401.json
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Product::paginate();
+        Validator::validate($request->all(), [
+            'per_page' => 'integer',
+            'page' => 'integer',
+        ]);
+
+        return Product::paginate($request->get('per_page', 10));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
