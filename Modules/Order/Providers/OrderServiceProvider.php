3e242e2dd281fd85534754e01cb566495ce185cd
Mahdi Mohammadi <lord2350.mm@gmail.com>
2023-06-27 21:01:03 +0330

    Init order module

---
+++ Modules/Order/Providers/OrderServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Order\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class OrderServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string $moduleName
+     */
+    protected string $moduleName = 'Order';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected string $moduleNameLower = 'order';
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(module_path($this->moduleName, 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides(): array
+    {
+        return [];
+    }
+}
---
+++ Modules/Order/Providers/RouteServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Modules\Order\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        parent::boot();
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api/order')
+            ->middleware(['api', 'auth:sanctum'])
+            ->group(module_path('Order', '/Routes/api.php'));
+    }
+}
